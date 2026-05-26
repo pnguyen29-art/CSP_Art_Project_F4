5be03bd4e5c84ffe9f985d836a32d9079ed6836c
mav28 <avu28@ssis.edu.vn>
2026-05-26 10:50:03 +0700

    house and roof my_picture.py

--- my_picture.py
+++ my_picture.py
@@ -19,6 +19,14 @@ def draw_picture(width, height):
     sg.fill_rectangle(375, 340,100, 25)
     sg.fill_rectangle(350, 360,100, 25)
     sg.fill_rectangle(325, 380,100, 25)
+    
+    sg.set_outline_color("black")
+    sg.set_fill_color ("pink")
+    sg.fill_rectangle(240, 200,160, 80)
+    
+    sg.set_outline_color("black")
+    sg.set_fill_color("brown")
+    sg.fill_triangle (220,200,320,140,420,200)
 
 
 if __name__ == "__main__":
