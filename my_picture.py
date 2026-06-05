9393557ec87c65aa6bac4b540221535f2a8be11d
pnguyen29-art <pnguyen29@ssis.edu.vn>
2026-06-05 08:13:42 +0700

    my_picture.py

--- my_picture.py
+++ my_picture.py
@@ -1,5 +1,6 @@
 import simple_graphics as sg
 
+    #Houses (Made by Minh An)
 def draw_house(x, y, color):
     """
     Draws a parameterized house with a base, roof, and door using public sg commands.
@@ -17,7 +18,8 @@ def draw_house(x, y, color):
     # 3. House Door
     sg.set_fill_color("brown")
     sg.fill_rectangle(x + 60, y + 20, 40, 60)
-
+    
+    #Birds (Made by Minh An)
 def draw_bird(x, y, color):
     """
     Draws a simple V-shaped bird flying in the sky using public sg commands.
@@ -27,7 +29,7 @@ def draw_bird(x, y, color):
     sg.draw_line(x - 15, y - 8, x, y)
     sg.draw_line(x, y, x + 15, y - 8)
     
-#cloud
+#cloud (Made by Jiwon)
 def draw_cloud(x, y):
     sg.set_outline_color("white")
     sg.set_fill_color("white")
@@ -41,6 +43,7 @@ def draw_cloud(x, y):
     sg.fill_arc(x+20,y-40,40,40,90,-90)
     sg.fill_arc(x+20,y,40,40,0,-90)
 
+    #Flowers (Made by Jiwon)
 def draw_flower(x,y):
     sg.set_outline_color("#77FF57")
     sg.set_fill_color("#77FF57")
@@ -55,7 +58,7 @@ def draw_flower(x,y):
     sg.set_fill_color("yellow")
     sg.fill_circle(x,y,5)
     
-#Person
+#Person (Made by Phong)
 def draw_blocky_person(x, y, height, color):
     """
     Draws a simple blocky 2D person.
@@ -94,7 +97,7 @@ def draw_blocky_person(x, y, height, color):
     # Right Arm
     sg.fill_rectangle(x + unit * 3.7, y + head_size, limb_w, body_h)
  
-#Dog
+#Dog (Made by Phong)
 def draw_blocky_dog(x, y, height, color):
     """
     Draws a simple blocky 2D dog looking to the right.
@@ -161,14 +164,15 @@ def draw_picture(width, height):
     # make some variables available
     colors = ["red", "green", "blue", "cyan", "magenta", "yellow"]
     
-    #Ground
+    #Sky (Made by Jiwon)
+    sg.fill_background("cyan")
+    
+    #Ground (Made by Phong)
     sg.set_outline_color("green")
     sg.set_fill_color ("green")
     sg.fill_rectangle(0, 280,600, 400)
-    sg.set_fill_color("cyan")
-    sg.fill_rectangle(0, 0, 600, 280)
     
-    #Lake
+    #Lake (Made by Phong)
     sg.set_fill_color("blue")
     sg.set_outline_color("blue")
     sg.fill_rectangle(450, 280,100, 25)
@@ -178,33 +182,33 @@ def draw_picture(width, height):
     sg.fill_rectangle(350, 360,100, 25)
     sg.fill_rectangle(325, 380,100, 25)
     
-    #clouds
+    #clouds (Made by Jiwon)
     draw_cloud(120,70)
     draw_cloud(320,65)
     
-    #House (Base)
+    #House (Base) (Made by Minh An)
     sg.set_outline_color("black")
     sg.set_fill_color ("pink")
     sg.fill_rectangle(240, 200,160, 80)
     
-    #House (Roof)
+    #House (Roof)(Made by Phong)
     sg.set_outline_color("black")
     sg.set_fill_color("brown")
     sg.fill_triangle (220,200,320,140,420,200)
     sg.set_outline_color("brown")
     
-    #House (Door)
+    #House (Door) (Made by Phong)
     sg.set_fill_color("brown")
     sg.fill_rectangle(300, 220, 40, 60)
     
-    #Tree
+    #Tree (Made by Phong)
     sg.set_fill_color ("brown")
     sg.fill_rectangle(75, 180, 25, 100)
     sg.set_fill_color ("green")
     sg.set_outline_color("green")
     sg.fill_rectangle (38, 100, 100, 100)
     
-    #Sun
+    #Sun (Made by Phong)
     sg.set_fill_color ("yellow")
     sg.fill_circle(600, 0, 100)
     
@@ -212,19 +216,21 @@ def draw_picture(width, height):
     draw_blocky_person (560, 230, 50, "blue")
     draw_blocky_person (200, 230, 50, "red")
     
-    #Dog
+    #House (Made by Minh An)
+    draw_house(240, 200, "pink")
+    
+    #Dog (Made by Phong
     draw_blocky_dog(150, 250, 30, "grey")
     draw_blocky_dog(380, 250, 30, "white")
 
-    #Flower
+    #Flower (Made by Jiwon)
     draw_flower(200,340)
     draw_flower(50,300)
     draw_flower(120,320)
     draw_flower(60,360)
     draw_flower(300,350)
 
-    draw_house(240, 200, "pink")
-    
+    #Bird
     draw_bird(220, 80, "white")
 if __name__ == "__main__":
     # Launch the wrapper; only edit starting dimensions of canvas if you would like to
